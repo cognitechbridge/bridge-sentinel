8785bc1f9e7c795cf8c02603def32e730d6823e6
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-03-21 23:11:08 -0700

    Mount button

--- src/lib/components/docs/dashboard/DashboardPage.svelte
+++ src/lib/components/docs/dashboard/DashboardPage.svelte
@@ -69,12 +69,6 @@
   <div class="flex-1 space-y-4 p-8 pt-6">
     <div class="flex items-center justify-between space-y-2">
       <h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
-      <div class="flex items-center space-x-2">
-        <Button size="sm">
-          <Download class="mr-2 h-4 w-4" />
-          Download
-        </Button>
-      </div>
     </div>
     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
       <div class="col-span-2 space-y-2">
