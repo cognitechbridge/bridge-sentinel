5a45c803e154ffbeafab188fc4c559e59ff975f0
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-03-23 17:37:49 -0700

    Move shortenFilePath to utils

--- src/lib/components/docs/dashboard/DashboardPage.svelte
+++ src/lib/components/docs/dashboard/DashboardPage.svelte
@@ -11,6 +11,7 @@
   import { saveRepositories, loadRepositories, generateRandomString } from '$api/app';
   import { onMount } from 'svelte';
   import RepositorySettings from './RepositorySettings.svelte';
+  import { shortenFilePath } from '$api/utils';
 
   let repositories: Repository[] = [];
   let selectedRepository: Repository | null = null;
@@ -37,46 +38,6 @@
   const selectRepository = (repository: Repository) => {
     selectedRepository = repository;
   };
-
-  function shortenFilePath(filePath: string, maxLength: number = 45): string {
-    if (filePath.length <= maxLength) {
-      return filePath;
-    }
-    const separator = filePath.includes('/') ? '/' : '\\';
-    const segments = filePath.split(separator);
-
-    // Handle cases where path length is too short to be meaningfully shortened
-    if (segments.length < 3) {
-      return filePath.slice(0, maxLength - 3) + separator + '...';
-    }
-
-    let start = segments[0];
-    let end = segments[segments.length - 1];
-
-    // Include the leading separator for absolute paths
-    if (filePath.startsWith(separator)) {
-      start = separator + start;
-    }
-
-    let middle = '...';
-
-    // Adjust according to the maximum length allowed
-    const startMaxLength = Math.ceil((maxLength - middle.length - end.length) / 2);
-    const endMaxLength = Math.floor((maxLength - middle.length - start.length) / 2);
-
-    console.log(startMaxLength, endMaxLength);
-
-    if (start.length > startMaxLength) {
-      start = start.slice(0, startMaxLength - 1) + '…';
-    }
-
-    if (end.length > endMaxLength) {
-      end = '…' + end.slice(-endMaxLength + 1);
-    }
-
-    // Reassemble the path using the determined separator
-    return start + separator + middle + separator + end;
-  }
 
   function handleRemove(event: CustomEvent<Repository>) {
     const repository = event.detail;
