0a2052eb5be5485a1a35c9c8b547ee9e83392c54
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-03-24 20:32:17 -0700

    Implement secret page

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,16 +4,57 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::Once;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn set_secret(secret: &str) -> bool {
+    let app = get_ui_app();
+    app.set_secret(secret.to_string());
+    true
+}
+
+static mut UI_APP: Option<UiApp> = None;
+static INIT: Once = Once::new();
+
+fn get_ui_app() -> &'static mut UiApp {
+    unsafe {
+        INIT.call_once(|| {
+            UI_APP = Some(UiApp::new());
+        });
+        UI_APP.as_mut().unwrap()
+    }
+}
+
+struct UiApp {
+    secret: String,
+}
+
+impl UiApp {
+    fn new() -> Self {
+        Self {
+            secret: "".to_string(),
+        }
+    }
+
+    fn run(&self) {
+        tauri::Builder::default()
+            .plugin(tauri_plugin_store::Builder::default().build())
+            .invoke_handler(tauri::generate_handler![set_secret])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
+
+    fn set_secret(&mut self, secret: String) {
+        self.secret = secret;
+    }
+
+    fn get_secret(&self) -> &str {
+        &self.secret
+    }
 }
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let ui_app = UiApp::new();
+    ui_app.run();
 }
