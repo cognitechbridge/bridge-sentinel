b1e93320acbfe61c03ff4c6d8faf909358438e96
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-03-29 22:03:50 -0700

    Implement check secret

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -29,17 +29,26 @@ impl UiApp {
         Self { key: vec![0, 32] }
     }
 
-    // Derives a key from the secret and sets it.
+    /// Derives a key from the secret and sets it.
     pub fn set_new_secret(&mut self, secret: &str, salt: &str) -> Result<String> {
         self.key = Self::derive_key_from_secret(secret, salt.as_bytes())?;
         let hash_secret = Self::hash_secret(secret)?;
         Ok(hash_secret)
     }
 
-    // Hashes a secret using Argon2id.
-    pub fn hash_secret(secret: &str) -> Result<String> {
-        let params = Params::new(64 * 1024, 2, 8, Some(32)).unwrap();
-        let argon2 = Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, params);
+    /// Checks if the secret matches the stored hash.
+    pub fn check_secret(&self, secret: &str, hash: &str) -> Result<bool> {
+        let argon2 = Argon2::default();
+        let password_hash = PasswordHash::new(hash).unwrap();
+        let is_valid = argon2
+            .verify_password(secret.as_bytes(), &password_hash)
+            .is_ok();
+        Ok(is_valid)
+    }
+
+    /// Hashes a secret using Argon2id.
+    fn hash_secret(secret: &str) -> Result<String> {
+        let argon2 = Self::generate_argon()?;
 
         let salt = SaltString::generate(&mut OsRng);
         let password_hash = argon2
@@ -51,12 +60,18 @@ impl UiApp {
 
     /// Derives a key from the secret using Argon2id.
     fn derive_key_from_secret(secret: &str, salt: &[u8]) -> Result<Vec<u8>> {
-        let params = Params::new(64 * 1024, 2, 8, Some(32)).unwrap();
-        let argon2 = Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, params);
+        let argon2 = Self::generate_argon()?;
         let mut key = vec![0; 32];
         if let Err(err) = argon2.hash_password_into(secret.as_bytes(), salt, &mut key) {
             panic!("Error hashing password: {:?}", err);
         }
         Ok(key)
+    }
+
+    /// Generates an Argon2 instance with the recommended parameters.
+    fn generate_argon() -> Result<Argon2<'static>> {
+        let params = Params::new(64 * 1024, 2, 8, Some(32)).unwrap();
+        let argon2 = Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, params);
+        Ok(argon2)
     }
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,28 +9,34 @@ use tauri_plugin_store::StoreCollection;
 pub mod app;
 
 #[tauri::command]
-fn set_secret(secret: &str, salt: &str, app_handle: tauri::AppHandle) -> bool {
+fn set_new_secret(secret: &str, salt: &str) -> String {
     let app = app::get_ui_app();
 
     let hashed_secret = app.set_new_secret(secret, salt).unwrap();
-
-    let stores = app_handle.state::<StoreCollection<Wry>>();
-    let path = PathBuf::from("config.json");
-    with_store(app_handle.clone(), stores, path, |store| {
-        store
-            .insert("hashed_secret".to_string(), json!(hashed_secret))
-            .unwrap();
-        store.save()
-    })
-    .unwrap();
-
-    true
+    hashed_secret.to_string()
 }
+
+#[tauri::command]
+fn check_secret(secret: &str, hash: &str) -> bool {
+    let app = app::get_ui_app();
+    app.check_secret(secret, hash).unwrap()
+}
+
+// #[tauri::command]
+// fn sample(secret: &str, salt: &str, app_handle: tauri::AppHandle) {
+//     let stores = app_handle.state::<StoreCollection<Wry>>();
+//     let path = PathBuf::from("config.json");
+//     with_store(app_handle.clone(), stores, path, |store| {
+//         store.insert("a".to_string(), json!("b")).unwrap();
+//         store.save()
+//     })
+//     .unwrap();
+// }
 
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![set_secret])
+        .invoke_handler(tauri::generate_handler![set_new_secret, check_secret])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
