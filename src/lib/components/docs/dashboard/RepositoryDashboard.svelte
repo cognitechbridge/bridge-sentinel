5a7987d08aa93ad58141c0e944851e38ea5dda13
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-04-13 10:36:27 -0700

    refactor: Remove hardcoded user avatar and information from RepositoryDashboard component

--- src/lib/components/docs/dashboard/RepositoryDashboard.svelte
+++ src/lib/components/docs/dashboard/RepositoryDashboard.svelte
@@ -63,18 +63,6 @@
 <CardContent>
   <div class="space-y-8">
     <ShareKey shareCode={repository?.status.public_key} />
-
-    <div class="flex items-center">
-      <Avatar class="h-9 w-9">
-        <AvatarImage src="/avatars/01.png" alt="Avatar" />
-        <AvatarFallback>OM</AvatarFallback>
-      </Avatar>
-      <div class="ml-4 space-y-1">
-        <p class="text-sm font-medium leading-none">Olivia Martin</p>
-        <p class="text-sm text-muted-foreground">olivia.martin@email.com</p>
-      </div>
-      <div class="ml-auto font-medium">+$1,999.00</div>
-    </div>
   </div>
 </CardContent>
 <ShareDialog bind:open={shareDialogOpen} path="" {repository} />
