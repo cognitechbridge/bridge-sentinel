ab7abcb379765fea6549bea178ecafe99e4a8e00
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-04-14 12:04:31 -0700

    ✨Do not close share dialog on ushare alert

--- src/lib/components/docs/dialogs/share-dialog/ShareDialog.svelte
+++ src/lib/components/docs/dialogs/share-dialog/ShareDialog.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-  import { app, type Repository } from '$api/app';
+  import { app, type AccessList, type Repository, type AppResult } from '$api/app';
   import ValidatedInput from '$components/ui/input/ValidatedInput.svelte';
+  import Separator from '$components/ui/separator/Separator.svelte';
+  import Table from '$components/ui/table/Table.svelte';
+  import TableBody from '$components/ui/table/TableBody.svelte';
+  import TableCell from '$components/ui/table/TableCell.svelte';
+  import TableHead from '$components/ui/table/TableHead.svelte';
+  import TableHeader from '$components/ui/table/TableHeader.svelte';
+  import TableRow from '$components/ui/table/TableRow.svelte';
+  import { Trash2 } from 'lucide-svelte';
   import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
   import {
     Dialog,
@@ -12,6 +20,13 @@
   } from '$lib/components/ui/dialog/index.js';
   import { Label } from '$lib/components/ui/label/index.js';
   import { toast } from 'svelte-sonner';
+  import UnshareDialog from '../unshare-dialog/UnshareDialog.svelte';
+
+  type RenderedAccessList = {
+    PublicKey: string;
+    Inherited: string;
+    Removable: boolean;
+  }[];
 
   async function list_access(path: string) {
     if (!repository) return;
@@ -20,18 +35,7 @@
   }
 
   async function share() {
-    repository =
-      app.repositories.find(
-        (repo) => repo.mountPoint && repo.mountPoint === path.slice(0, repo.mountPoint.length)
-      ) || null;
-
-    if (!repository) {
-      toast.error('Invalid Path', {
-        description: 'The path not found in the mounted repositories'
-      });
-      return;
-    }
-
+    if (!repository) return;
     let res = await app.sharePath(repository?.path, path, publicKey);
     if (res.ok) {
       open = false;
@@ -45,10 +49,48 @@
     }
   }
 
+  let openUnshare = false;
+  let unsharePublicKey = '';
+  async function openUnshareDialog(public_key: string) {
+    unsharePublicKey = public_key;
+    openUnshare = true;
+  }
+
+  async function unshare() {
+    if (!repository) return;
+    let res = await app.unsharePath(repository?.path, path, unsharePublicKey);
+    if (res.ok) {
+      toast.success('Path unshared successfully', {
+        description: 'The path has been unshared with the user'
+      });
+    } else {
+      toast.error('Failed to unshare path', {
+        description: 'Please try again'
+      });
+    }
+  }
+
+  function cancelUnshare() {
+    open = true;
+  }
+
+  function render_access_list(accessList: AccessList): RenderedAccessList {
+    let res = accessList.map((access) => {
+      let publickey = repository?.status.public_key == access.PublicKey ? 'You' : access.PublicKey;
+      let removable = access.PublicKey != repository?.status.public_key;
+      return {
+        PublicKey: publickey,
+        Inherited: access.Inherited ? 'Yes' : 'No',
+        Removable: removable
+      };
+    });
+    return res;
+  }
+
   let mountPoint = '';
   let publicKey = '';
   let pathErr = '';
-  let accessList: any = {};
+  let accessList: RenderedAccessList = [];
 
   export let repository: Repository | null = null;
   export let open = false;
@@ -56,18 +98,23 @@
 
   $: mountPoint = repository?.mountPoint || '';
   $: {
-    list_access(path).then((res) => (accessList = res));
-    if (!accessList || accessList.err) {
-      pathErr = 'The path is not valid';
-    } else {
-      pathErr = '';
-    }
+    open &&
+      repository &&
+      list_access(path).then((res) => {
+        if (!res || res.err) {
+          pathErr = 'The path is not valid';
+          accessList = [];
+        } else {
+          pathErr = '';
+          accessList = render_access_list(res.result);
+        }
+      });
   }
   $: isValid = !pathErr && publicKey.length > 0;
 </script>
 
 <Dialog bind:open>
-  <DialogContent class="sm:max-w-[425px]">
+  <DialogContent class="sm:max-w-[800px]">
     <DialogHeader>
       <DialogTitle>Share path</DialogTitle>
       <DialogDescription>
@@ -87,5 +134,38 @@
     <DialogFooter>
       <Button type="submit" on:click={share} disabled={!isValid}>Share</Button>
     </DialogFooter>
+    <Separator class="my-1" />
+    <!-- ------------------------ Table ---------------------------- -->
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>#</TableHead>
+          <TableHead>Public Key</TableHead>
+          <TableHead>Inherited</TableHead>
+          <TableHead>Remove</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {#each accessList as access, i (i)}
+          <TableRow>
+            <TableCell>{i + 1}</TableCell>
+            <TableCell>{access.PublicKey}</TableCell>
+            <TableCell>{access.Inherited}</TableCell>
+            <TableCell>
+              {#if access.Removable}
+                <Button
+                  variant="outline"
+                  class="border-none bg-transparent hover:bg-transparent h-0"
+                  on:click={() => openUnshareDialog(access.PublicKey)}
+                >
+                  <Trash2 class="h-4 w-4 text-red-600" />
+                </Button>
+              {/if}
+            </TableCell>
+          </TableRow>
+        {/each}
+      </TableBody>
+    </Table>
   </DialogContent>
 </Dialog>
+<UnshareDialog bind:open={openUnshare} on:unshare={unshare} on:cancel={cancelUnshare} />
