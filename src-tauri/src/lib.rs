42841e91e2d68ee2c3bb8f39eaf0787a3cab9df4
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-05-23 21:23:04 -0700

    Fix build warnings

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -175,10 +175,10 @@ where
 /// Runs the Tauri application.
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
-                let window = app.get_window("main").unwrap();
+                let window = _app.get_window("main").unwrap();
                 window.open_devtools();
                 window.close_devtools();
             }
--- src/routes/login-cloud/+page.svelte
+++ src/routes/login-cloud/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import LoginWebPage from '$components/pages/login-cloud/LoginWebPage.svelte';
-  import type { PageData } from './$types';
-
-  export let data: PageData;
 </script>
 
 <LoginWebPage />
