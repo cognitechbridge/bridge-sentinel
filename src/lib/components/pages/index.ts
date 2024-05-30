8212e5881e3fd9cd26a236045da7fc946a50cf34
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-05-29 22:05:41 -0700

    feat: Move first time page to routes

--- src/lib/components/pages/index.ts
+++ src/lib/components/pages/index.ts
@@ -3,4 +3,3 @@ export { default as TailwindIndicator } from './TailwindIndicator.svelte';
 export * from './dashboard';
 export * from './register';
 export * from './register-cloud';
-export * from './first-time';
